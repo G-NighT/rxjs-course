0b45c31ec97f875e98a402f7c7eafcddafb19d2e
Minligareev Maxim Alexandrovich <m-a_a130@mail.ru>
2023-08-23 20:34:47 +0700

    Update about.component.ts

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,40 +1,57 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import {
-    concat,
-    fromEvent,
-    interval,
-    noop,
-    observable,
-    Observable,
-    of,
-    timer,
-    merge,
-    Subject,
-    BehaviorSubject,
-    AsyncSubject,
-    ReplaySubject
-} from 'rxjs';
-import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
-import {createHttpObservable} from '../common/util';
-
+  concat,
+  fromEvent,
+  interval,
+  noop,
+  observable,
+  Observable,
+  of,
+  timer,
+  merge,
+  Subject,
+  BehaviorSubject,
+  AsyncSubject,
+  ReplaySubject,
+} from "rxjs";
+import { delayWhen, filter, map, take, timeout } from "rxjs/operators";
+import { createHttpObservable } from "../common/util";
 
 @Component({
-    selector: 'about',
-    templateUrl: './about.component.html',
-    styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
+  ngOnInit() {
+    //1
+    const interval$ = timer(3000, 1000);
 
-    ngOnInit() {
+    interval$.subscribe(
+      (val) => console.log(val),
+      (err) => console.log(err),
+      () => console.log("complete")
+    );
 
+    //2
+    const http$ = Observable.create((observer) => {
+      fetch("/api/courses")
+        .then((response) => {
+          return response.json();
+        })
+        .then((body) => {
+          observer.next(body);
+          observer.complete();
+        })
+        .catch((error) => {
+          observer.error(error);
+        });
+    });
 
-    }
-
-
+    http$.subscribe(
+      (courses) => console.log(courses),
+      (error) => console.log(error),
+      () => console.log("complete")
+    );
+  }
 }
-
-
-
-
-
-
